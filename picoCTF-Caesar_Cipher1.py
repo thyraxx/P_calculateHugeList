82dc085fca8070d1803ed61de3d59e4f29a7db9f
thyraxx <taz3008@msn.com>
2018-11-23 13:55:04 +0100

    Update picoCTF-Caesar_Cipher1.py

--- picoCTF-Caesar_Cipher1.py
+++ picoCTF-Caesar_Cipher1.py
@@ -1,14 +1,15 @@
 # Made for picoCTF challenge, caesar cipher1
-# Could've used just an online tool, but where's the fun in that
+# Could've just used an online tool, but where's the fun in that.
 import sys
 
 cipher = sys.argv[1]
 answer = ""
-offsetArg = 0
 
 # if no offset is given, let's try them all!
 if len(sys.argv) == 3:
 	offsetArg = int(sys.argv[2])
+else:
+	offsetArg = 0
 
 def realIndex(letter, offset):
 	global answer
@@ -20,41 +21,43 @@ def realIndex(letter, offset):
 
 listAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 if offsetArg == 0:
+	start = 0
 	i = 26
 else:
-	i = offsetArg
+	start = offsetArg
+	i = offsetArg + 1
+	
 
-for offset in range(0, i):
+for offset in range(start, i):
 	for letter in cipher:
 		realIndex(letter, offset)
 	print offset, answer.lower()
 	answer = ""
 
-# python pythonfile shiftedtext OPTIONAL n iterations
 # python picoCTF-caesar_cipher1.py yjhipvddsdasrpthpgrxewtgdqnjytto
 
-# 0 yjhipvddsdasrpthpgrxewtgdqnjytto
-# 1 zkijqweetebtsquiqhsyfxuherokzuup
-# 2 aljkrxffufcutrvjritzgyvifsplavvq
-# 3 bmklsyggvgdvuswksjuahzwjgtqmbwwr
-# 4 cnlmtzhhwhewvtxltkvbiaxkhurncxxs
-# 5 domnuaiixifxwuymulwcjbylivsodyyt
-# 6 epnovbjjyjgyxvznvmxdkczmjwtpezzu
-# 7 fqopwckkzkhzywaownyeldankxuqfaav
-# 8 grpqxdllaliazxbpxozfmebolyvrgbbw
-# 9 hsqryemmbmjbaycqypagnfcpmzwshccx
-# 10 itrszfnncnkcbzdrzqbhogdqnaxtiddy
-# 11 justagoodoldcaesarcipherobyujeez <-- Correct one
-# 12 kvtubhppepmedbftbsdjqifspczvkffa
-# 13 lwuvciqqfqnfecguctekrjgtqdawlggb
-# 14 mxvwdjrrgrogfdhvduflskhurebxmhhc
-# 15 nywxeksshsphgeiwevgmtlivsfcyniid
-# 16 ozxyflttitqihfjxfwhnumjwtgdzojje
-# 17 payzgmuujurjigkygxiovnkxuheapkkf
-# 18 qbzahnvvkvskjhlzhyjpwolyvifbqllg
-# 19 rcabiowwlwtlkimaizkqxpmzwjgcrmmh
-# 20 sdbcjpxxmxumljnbjalryqnaxkhdsnni
-# 21 tecdkqyynyvnmkockbmszrobylietooj
-# 22 ufdelrzzozwonlpdlcntaspczmjfuppk
-# 23 vgefmsaapaxpomqemdoubtqdankgvqql
-# 24 whfgntbbqbyqpnrfnepvcurebolhwrrm
+# 0 yjhipvddsdasrpthpgrxewtg dqnjytto
+# 1 zkijqweetebtsquiqhsyfxuh erokzuup
+# 2 aljkrxffufcutrvjritzgyvi fsplavvq
+# 3 bmklsyggvgdvuswksjuahzwj gtqmbwwr
+# 4 cnlmtzhhwhewvtxltkvbiaxk hurncxxs
+# 5 domnuaiixifxwuymulwcjbyl ivsodyyt
+# 6 epnovbjjyjgyxvznvmxdkczm jwtpezzu
+# 7 fqopwckkzkhzywaownyeldan kxuqfaav
+# 8 grpqxdllaliazxbpxozfmebo lyvrgbbw
+# 9 hsqryemmbmjbaycqypagnfcp mzwshccx
+# 10 itrszfnncnkcbzdrzqbhogdq naxtiddy
+# 11 justagoodoldcaesarcipher obyujeez <-- Correct one
+# 12 kvtubhppepmedbftbsdjqifs pczvkffa
+# 13 lwuvciqqfqnfecguctekrjgt qdawlggb
+# 14 mxvwdjrrgrogfdhvduflskhu rebxmhhc
+# 15 nywxeksshsphgeiwevgmtliv sfcyniid
+# 16 ozxyflttitqihfjxfwhnumjw tgdzojje
+# 17 payzgmuujurjigkygxiovnkx uheapkkf
+# 18 qbzahnvvkvskjhlzhyjpwoly vifbqllg
+# 19 rcabiowwlwtlkimaizkqxpmz wjgcrmmh
+# 20 sdbcjpxxmxumljnbjalryqna xkhdsnni
+# 21 tecdkqyynyvnmkockbmszrob ylietooj
+# 22 ufdelrzzozwonlpdlcntaspc zmjfuppk
+# 23 vgefmsaapaxpomqemdoubtqd ankgvqql
+# 24 whfgntbbqbyqpnrfnepvcure bolhwrrm
